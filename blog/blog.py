2801462167f57ceaab43f8fd8748fbe862097fdc
WillCoates <starrydoesgaming@gmail.com>
2015-11-24 15:56:38 +0000

    Update blog.py

--- blog/blog.py
+++ blog/blog.py
@@ -13,6 +13,10 @@ def home():
 def posts(name):
     return render_template('posts/%s.html' % name)
 
+@app.route('/test')
+def testpost():
+    return render_template('viewpost.html', title='Test page', post=['This is a test page', 'It is to test a template and nothing more'])
+
 if __name__ == '__main__':
     app.secret_key = getenv('SessionKey')
     app.run(debug=True, host='0.0.0.0')
